cd224d6781386b937eb0e523e5213be2b4264a28
naveen <usre@manoj>
2019-04-27 08:35:55 +0530

    Program

---
+++ src/naveenkumarkb/studentmarks.java
@@ -0,0 +1,14 @@
+package naveenkumarkb;
+
+public class studentmarks {
+	public static void main(String arg[]) {
+		float percentage=50f;
+		if(percentage>=50f) {
+			System.out.println("Qualified");
+		}
+		else {
+			System.out.println("Disqualified");
+		}
+	}
+
+}
