8add6c179586cd7dc60e0340bdabd3843aededcc
usre <usre@manoj>
2019-04-27 08:21:14 +0530

    example

---
+++ src/manoj/studentmarks.java
@@ -0,0 +1,15 @@
+package manoj;
+
+public class studentmarks {
+
+	public static void main(String args[]) {
+		float percentage=50;
+		if(percentage>=50){
+			System.out.println("Qualified");
+		}
+		else {
+			System.out.println("Disqualified");
+		}
+		
+	}
+}
