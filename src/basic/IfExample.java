70e237a8576de549d6ea2da4abab54ba3a9387d5
bharath kristipati <bharath.kristipati@gmail.com>
2019-04-27 07:52:32 +0530

    If example

---
+++ src/basic/IfExample.java
@@ -0,0 +1,17 @@
+package basic;
+
+public class IfExample {
+
+	
+	public static void main(String args[]) {
+		
+		int marks = 50;
+		
+		if(marks>=40) {
+			System.out.println("PASS");
+		} else {
+			System.out.println("FAIL");
+		}
+		
+	}
+}
