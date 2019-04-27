f43dfd9368b6f821bce4bfa6b0aaf3f442490da0
bharath kristipati <bharath.kristipati@gmail.com>
2019-04-27 08:33:54 +0530

    Update if class

--- src/basic/IfExample.java
+++ src/basic/IfExample.java
@@ -5,11 +5,27 @@ public class IfExample {
 	
 	public static void main(String args[]) {
 		
-		int marks = 50;
+		int marks = 110;
 		
 		if(marks>=40) {
-			System.out.println("PASS");
+			if(marks>100){
+				System.out.println("In valid");
+			}else {
+				System.out.println("PASS");
+			}
 		} else {
+			if(marks<0){
+				System.out.println("In valid");
+			}else {
+				System.out.println("FAIL");
+			}
+		} 
+		
+		if(marks < 0 || marks>100) {
+			System.out.println("INVALID");
+		}else if(marks>40){
+			System.out.println("PASS");
+		}else {
 			System.out.println("FAIL");
 		}
 		
