821af0c4ebda1d11efdcd93743b0a2c53134d168
naveen <Lenovo@Lenovo-PC>
2019-05-01 23:51:24 +0530

    Object, Method Practice

---
+++ src/naveenkumarkb/ObjectClass.java
@@ -0,0 +1,24 @@
+package naveenkumarkb;
+
+public class ObjectClass {
+	public void add() {
+		int a=1;
+		int b=2;
+		int c=a+b;
+	System.out.println(c);
+	}
+	public void sub(int x) {
+		int y=3;
+		int z=x-y;
+	System.out.println(z);
+	}
+	public void div(int t) {
+		int u=2;
+		int v=t/u;
+	System.out.println(v);
+	}
+	public void mul(int k, int l) {
+		int mul=k*l;
+	System.out.println(mul);
+	}
+}
---
+++ src/naveenkumarkb/ReturnClass.java
@@ -0,0 +1,9 @@
+package naveenkumarkb;
+
+public class ReturnClass {
+	public static void main(String args[]) {
+		Return sum=new Return();
+		sum.add(3,4);
+	}
+
+}
